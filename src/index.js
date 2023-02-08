df75682ae65ebf5ee2ff627ad13db783eeb24fe8
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-02-08 15:48:13 -0500

    challenge 1-3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', ()=>{
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+        .then (resp => resp.json())
+        .then (posts => {
+            dogImage(posts)
+        })
+})
+
+function dogImage(object){
+    const arrayObj = object.message
+    arrayObj.forEach(dogUrl => {
+        const img = document.createElement('img')
+        img.src=dogUrl
+        document.getElementById("dog-image-container").append(img)
+    })  
+}
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    fetch("https://dog.ceo/api/breeds/list/all")
+        .then (resp => resp.json())
+        .then (dogBreedArray => {
+            addDogBreed(dogBreedArray)
+        })
+})
+
+function addDogBreed(breedObj){
+    const breedName = breedObj.message
+    const allBreeds = Object.keys(breedName)
+    allBreeds.forEach(listBreed =>{
+        const li = document.createElement('li')
+        li.innerText = listBreed
+        const dogBreed = document.getElementById("dog-breeds").append(li)
+        li.id = "dog-breed-name"
+        li.addEventListener('click', (e)=>{
+            li.style.color === 'blue' ? li.style.color ='black' : li.style.color = 'blue'
+        })
+    })
+}
